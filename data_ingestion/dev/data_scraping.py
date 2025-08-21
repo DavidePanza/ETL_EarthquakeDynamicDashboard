0c908179b118ff354d71fd1a2910e3aa220cc893
DavidePanza <davide.panza@gmail.com>
2025-08-21 10:29:21 +0200

    added data scraping script

--- data_ingestion/dev/data_scraping.py
+++ data_ingestion/dev/data_scraping.py
@@ -1,7 +1,7 @@
 import os
-import sys
 import boto3
 import pandas as pd
+from io import BytesIO
 from datetime import datetime, timedelta
 
 def lambda_handler(event, context):
@@ -26,16 +26,22 @@ def lambda_handler(event, context):
     
     print(f"Loading earthquake data for {start_date}...")
     df_earthquake = pd.read_csv(url)
-    df_earthquake['time'] = pd.to_datetime(df_earthquake['time'])
+
+    cols_to_keep = ['time', 'latitude', 'longitude', 'depth', 'mag', 'place', 'id']
+    df = df_earthquake[cols_to_keep].dropna().round(3)
+
+    df['timestamps'] = pd.to_datetime(df['time']).dt.round('s')
+    df['date'] = df['timestamps'].dt.date
+    df['time'] = df['timestamps'].dt.floor('s').dt.time
     
-    df_earthquake = df_earthquake
-    
-    if not df_earthquake.empty:
+    if not df.empty:
         print("Uploading data to S3...")
+        buffer = BytesIO()
+        df.to_parquet(buffer, index=False)
         s3_client.put_object(
             Bucket=bucket,
             Key=f"data/raw/earthquake_{start_date}.parquet",
-            Body=df_earthquake.to_parquet()
+            Body=buffer.getvalue()
         )
         print(f"Successfully uploaded data for {start_date}")
     else:
